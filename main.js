e018eea3aa04bad73a599b93e51ac4c4271812a8
Anirudh sahu <renudilip19@gmail.com>
2023-09-19 22:33:15 +0530

    complete 1.3

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var clutter = ''
 var time = 60
 var hit = 0
 var score = 0
 function makeBubble(){
-
+    var clutter = ''
+    console.log("abc");
     for (let i = 0; i < 126; i++) {
         let random = Math.floor(Math.random()*10)
         clutter += `<div class='bubble'> ${random} </div>`
@@ -11,15 +11,21 @@ function makeBubble(){
     document.querySelector("#btm").innerHTML = clutter
 }
 
-
 function hitValueUpdate(){
-    let random = Math.floor(Math.random()*10)
-    document.querySelector("#hitval").innerHTML = random
+    hit = Math.floor(Math.random()*10)
+    document.querySelector("#hitval").innerHTML = hit
 }
 
 function incScore(){
     score = score + 10
     document.querySelector('#score').innerHTML = score
+}
+
+function handleRestart(){
+    makeBubble()
+    time = 60
+    score = 0
+    hitValueUpdate()
 }
 
 function timer() {
@@ -31,12 +37,23 @@ function timer() {
         }
         else{
             clearInterval(timeinter)
-            document.querySelector("#btm").innerHTML = `<h2>Game Over </br> Your score is ${score}</h2>`
+            document.querySelector("#btm").innerHTML = `<h2>Game Over </br> Your score is ${score}</h2><button onclick="handleRestart()" class='restart'>Restart</button>`
 
         }
     }, 1000);
     
 }
+document.querySelector('#btm').addEventListener('click',function(dets){
+    var clicked_num = Number(dets.target.textContent)
+    if(time > 59){
+            timer()
+    }
+    if (clicked_num === hit){
+        incScore()
+        hitValueUpdate()
+        makeBubble()
+    }
+
+})
 makeBubble()
-timer()
 hitValueUpdate()

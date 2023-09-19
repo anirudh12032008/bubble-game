935fb1afbf7a950e9d05d447643f4c135d91e951
Anirudh sahu <renudilip19@gmail.com>
2023-09-19 14:38:53 +0530

    day -1.2

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 var clutter = ''
+var time = 60
+var hit = 0
+var score = 0
+function makeBubble(){
 
-for (let i = 0; i < 126; i++) {
-    random = Math.floor(Math.random()*10)
-    clutter += `<div class='bubble'> ${random} </div>`
+    for (let i = 0; i < 126; i++) {
+        let random = Math.floor(Math.random()*10)
+        clutter += `<div class='bubble'> ${random} </div>`
+    }
+    document.querySelector("#btm").innerHTML = clutter
 }
-var bottom = document.querySelector("#btm")
-bottom.innerHTML = clutter
+
+
+function hitValueUpdate(){
+    let random = Math.floor(Math.random()*10)
+    document.querySelector("#hitval").innerHTML = random
+}
+
+function incScore(){
+    score = score + 10
+    document.querySelector('#score').innerHTML = score
+}
+
+function timer() {
+    
+    var timeinter = setInterval(() => {
+        if (time>0) {
+            time--
+            document.querySelector('#time').innerHTML = time
+        }
+        else{
+            clearInterval(timeinter)
+            document.querySelector("#btm").innerHTML = `<h2>Game Over </br> Your score is ${score}</h2>`
+
+        }
+    }, 1000);
+    
+}
+makeBubble()
+timer()
+hitValueUpdate()
